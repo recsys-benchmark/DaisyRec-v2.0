c4fc2802898634925436f25d28b605edfd7a3d9d
amazingDD <1172392977@gmail.com>
2022-07-16 21:38:29 +0800

    remove unused packages

--- daisy/utils/splitter.py
+++ daisy/utils/splitter.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
-
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import KFold
 
 class TestSplitter(object):
     def __init__(self, config):
