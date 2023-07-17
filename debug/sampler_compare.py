8189d5cc2fbdd33979bdc8988d62a84e0add2b24
SZaheen60 <zaheen60@gmail.com>
2023-07-17 14:04:09 +0800

    moved debugging files to separate directory

---
+++ debug/sampler_compare.py
@@ -0,0 +1,79 @@
+from daisy.utils.sampler import BasicNegtiveSampler
+import time
+from sys import exit
+
+def sampler_compare(train_set, config):
+
+    for num_neg in 4, 5, 6, 7, 8, 9:
+        print("\n======================================")
+        print(f'Now calculating for {num_neg} negative samples per user-item pair:')
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.sampling()
+        e = time.time()
+        old_time = e-s
+        print(f'Prev sampling time: {round(old_time, 5)}')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.guess_and_check_sampling()
+        e = time.time()
+        guessandcheck_time = e-s
+        print(f'Guess and check sampling time: {round(guessandcheck_time,3)}s')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.set_diff_sampling()
+        e = time.time()
+        setdifftime = e-s
+        print(f'Set diff sampling time: {round(setdifftime, 4)}')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.batch_sampling(sampling_batch_size=64)
+        e = time.time()
+        new_64_time = e-s
+        print(f'Batch-aware sampling time: {round(new_64_time,3)}s (batch-size=64)')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.batch_sampling(sampling_batch_size=128)
+        e = time.time()
+        new_128_time = e-s
+        print(f'Batch-aware sampling time: {round(new_128_time,3)}s (batch-size=128)')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+        sampler = BasicNegtiveSampler(train_set.copy(), config)
+        sampler.num_ng = num_neg
+        s = time.time()
+        sampler.batch_sampling(sampling_batch_size=256)
+        e = time.time()
+        new_256_time = e-s
+        print(f'Batch-aware sampling time: {round(new_256_time,3)}s  (batch-size=256)')
+        assert len(sampler.df) == len(train_set) * num_neg
+        del sampler
+
+        print(f'\nFor {num_neg} negative items per user-item pair: ')
+        print(f'The guess-and-check sampling time is {round(guessandcheck_time/old_time, 2)} times slower')
+        print(f'The set difference  sampling time is {round(setdifftime/old_time, 2)} times slower')
+        print(f'The  64 batch-aware sampling time is {round(new_64_time/old_time, 2)} times slower')
+        print(f'The 128 batch-aware sampling time is {round(new_128_time/old_time, 2)} times slower')
+        print(f'The 256 batch-aware sampling time is {round(new_256_time/old_time, 2)} times slower')
+
+    exit()
--- test.py
+++ test.py
@@ -8,83 +8,8 @@ from daisy.utils.config import init_seed, init_config, init_logger
 from daisy.utils.sampler import BasicNegtiveSampler, SkipGramNegativeSampler
 from daisy.utils.dataset import get_dataloader, BasicDataset, CandidatesDataset, AEDataset
 from daisy.utils.utils import ensure_dir, get_ur, get_history_matrix, build_candidates_set, get_inter_matrix, get_ur_with_negatives
-from sys import exit
+from debug.sampler_compare import sampler_compare
 
-def sampler_compare(train_set, config):
-
-    for num_neg in 4, 5, 6, 7, 8, 9:
-        print("\n======================================")
-        print(f'Now calculating for {num_neg} negative samples per user-item pair:')
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.sampling()
-        e = time.time()
-        old_time = e-s
-        print(f'Prev sampling time: {round(old_time, 5)}')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.guess_and_check_sampling()
-        e = time.time()
-        guessandcheck_time = e-s
-        print(f'Guess and check sampling time: {round(guessandcheck_time,3)}s')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.set_diff_sampling()
-        e = time.time()
-        setdifftime = e-s
-        print(f'Set diff sampling time: {round(setdifftime, 4)}')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.batch_sampling(sampling_batch_size=64)
-        e = time.time()
-        new_64_time = e-s
-        print(f'Batch-aware sampling time: {round(new_64_time,3)}s (batch-size=64)')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.batch_sampling(sampling_batch_size=128)
-        e = time.time()
-        new_128_time = e-s
-        print(f'Batch-aware sampling time: {round(new_128_time,3)}s (batch-size=128)')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-        sampler = BasicNegtiveSampler(train_set.copy(), config)
-        sampler.num_ng = num_neg
-        s = time.time()
-        sampler.batch_sampling(sampling_batch_size=256)
-        e = time.time()
-        new_256_time = e-s
-        print(f'Batch-aware sampling time: {round(new_256_time,3)}s  (batch-size=256)')
-        assert len(sampler.df) == len(train_set) * num_neg
-        del sampler
-
-        print(f'\nFor {num_neg} negative items per user-item pair: ')
-        print(f'The guess-and-check sampling time is {round(guessandcheck_time/old_time, 2)} times slower')
-        print(f'The set difference  sampling time is {round(setdifftime/old_time, 2)} times slower')
-        print(f'The  64 batch-aware sampling time is {round(new_64_time/old_time, 2)} times slower')
-        print(f'The 128 batch-aware sampling time is {round(new_128_time/old_time, 2)} times slower')
-        print(f'The 256 batch-aware sampling time is {round(new_256_time/old_time, 2)} times slower')
-
-    exit()
 
 
 if __name__ == '__main__':
@@ -120,7 +45,6 @@ if __name__ == '__main__':
     total_train_ur = get_ur_with_negatives(train_set, item_num)
 
     config['train_ur'] = total_train_ur
-    sampler_compare(train_set, config)
 
     # TODO: REMOVE, FOR DEBUGGING ONLY
     sampler_compare(train_set, config)
